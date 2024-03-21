11352abc75b1771e199982ff2f754672668f5826
ganning127 <ganningxu@gmail.com>
2024-03-21 16:31:46 -0400

    only new activity for login with spotify

--- app/src/main/java/com/example/statsforspotify/Login.java
+++ app/src/main/java/com/example/statsforspotify/Login.java
@@ -1,7 +1,23 @@
 package com.example.statsforspotify;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    private String TAG = "LOGIN";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
+        Button loginWithSpotifyButton = findViewById(R.id.login_with_spotify);
+
+        loginWithSpotifyButton.setOnClickListener((v) -> {
+            Log.d(TAG, "onCreate: loginWithSpotifyButton clicked");
+        });
+    }
 }
